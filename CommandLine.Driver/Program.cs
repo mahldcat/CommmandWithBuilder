60761b7f38224b513fe85c0e4f2507a18f12de6f
mahldcat <mahldcat@gmail.com>
2024-11-27 09:52:40 -0700

    Initial Commit

---
+++ CommandLine.Driver/Program.cs
@@ -0,0 +1,59 @@
+﻿using CommandLine.Service;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+
+namespace CommandLine.Driver;
+
+public class Driver
+{
+    private const string DOTNET_ENV = "DOTNET_ENVIRONMENT";
+    private const string DEFAULT_ENV = "Development";
+
+    private static string InitializeEnvironment()
+    {
+        string environment = Environment.GetEnvironmentVariable(DOTNET_ENV) ??
+                             DEFAULT_ENV;
+        Environment.SetEnvironmentVariable(DOTNET_ENV,environment);
+
+        return environment;
+    }
+
+    
+    //private static 
+    public static async Task Main(string[] args)
+    {
+        InitializeEnvironment();
+
+        IHost host = CreateHostBuilder(args).Build();
+        await host.RunAsync();
+    }
+    
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var env = hostingContext.HostingEnvironment.EnvironmentName;
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables();
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<CommandLineAppService>();
+                /*
+                var env = hostContext.HostingEnvironment.EnvironmentName;
+                if (env == Environments.Development)
+                {
+                }
+                else if (env == "Test")
+                {
+                }
+                else if (env == Environments.Production)
+                {
+                }
+                */
+            });
+    
+}
---
+++ CommandLine.Service/CommandLineAppService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CommandLine.Service;
+
+public class CommandLineAppService(ILogger<CommandLineAppService> logger, IHostApplicationLifetime hostApplicationLifetime) : IHostedService
+{
+    private async Task CommandLineTask(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("CommandLineTask Started");
+        await Task.Delay(2000,cancellationToken);
+        logger.LogInformation("CommandLineTask Completed");
+    }
+    
+    #region IHostedService
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("StartAsync Started...");
+        try
+        {
+            await CommandLineTask(cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error in Execution of Task");
+        }
+        finally
+        {
+            hostApplicationLifetime.StopApplication();
+        }
+        logger.LogInformation("StartAsync Completed...");
+    }
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("StopAsync");
+        return Task.CompletedTask;
+    }
+    #endregion
+}
